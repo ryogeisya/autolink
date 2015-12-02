02cf20413cd098e55fcbd9edb8f001df5ba1f3ce
asihuretti <asihuretti@gmail.com>
2015-12-02 06:48:28 +0000

    autolink

--- lib/rails_autolink/helpers.rb
+++ lib/rails_autolink/helpers.rb
@@ -73,7 +73,7 @@ module RailsAutolink
 
           AUTO_LINK_RE = %r{
               (?: ((?:ed2k|ftp|http|https|irc|mailto|news|gopher|nntp|telnet|webcal|xmpp|callto|feed|svn|urn|aim|rsync|tag|ssh|sftp|rtsp|afs|file):)//)
-              [^\s<\u00A0\u2200-\u2300\u2500-\u2542\u3000-\u303F\uFF01-\uFF5D\uFFE3\u309B\u309C\u002C]+
+              [^\s<\u00A0\u2200-\u2300\u2500-\u2542\u3000-\u303F\uFF01-\uFF5D\uFFE3\u309B\u309C\u002C"]+
             }ix
 
           # regexps for determining context, used high-volume
